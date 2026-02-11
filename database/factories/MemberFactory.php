7f6ba94b1c53280c640d5e26653f46bafd33d0e8
EmunWebDev <e.felipe.537937@umindanao.edu.ph>
2026-02-11 23:35:43 +0800

    Fix MemberFactory mapping + unique emails

--- database/factories/MemberFactory.php
+++ database/factories/MemberFactory.php
@@ -2,10 +2,13 @@
 
 namespace Database\Factories;
 
+use App\Models\Member;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 class MemberFactory extends Factory
 {
+    protected $model = Member::class;
+
     public function definition(): array
     {
         $first = $this->faker->firstName();
@@ -14,26 +17,30 @@ class MemberFactory extends Factory
         return [
             'image' => null,
             'first_name' => $first,
-            'middle_name' => $this->faker->lastName(),
+            'middle_name' => $this->faker->optional()->firstName(), // better than lastName
             'last_name' => $last,
             'suffix' => null,
-            // full_name is virtual, do NOT include it
+            // don't include full_name; it will be set by Member::booted() saving hook
+
             'date_of_birth' => $this->faker->date(),
             'gender' => $this->faker->randomElement(['Male', 'Female']),
             'marital_status' => $this->faker->randomElement(['Single', 'Married', 'Widowed', 'Separated']),
             'complete_address' => $this->faker->address(),
-            'email_address' => $this->faker->safeEmail(),
-            'phone_no' => '09' . $this->faker->numerify('#########'), // 11 digits
+
+            'email_address' => $this->faker->unique()->safeEmail(), // important
+            'phone_no' => '09' . $this->faker->numerify('#########'),
             'facebook_account' => strtolower($first . $last),
             'occupation' => $this->faker->jobTitle(),
             'membership_status' => $this->faker->randomElement(['Active', 'Inactive', 'Transferred']),
             'membership_date' => $this->faker->date(),
-            'baptism_date' => $this->faker->date(),
+            'baptism_date' => $this->faker->optional()->date(),
             'baptism_location' => $this->faker->city(),
+
             'emergency_contact_name' => $this->faker->name(),
             'emergency_contact_phone_no' => '09' . $this->faker->numerify('#########'),
             'emergency_contact_relation' => $this->faker->randomElement(['Father', 'Mother']),
-            'user_id' => 1, // fixed
+
+            'user_id' => 1,
         ];
     }
 }
