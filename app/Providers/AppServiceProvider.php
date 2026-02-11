44cad1d16fcdb08eeb6f726df54b911668f6eb2f
EmunWebDev <e.felipe.537937@umindanao.edu.ph>
2026-02-11 23:15:10 +0800

    Force HTTPS scheme in production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,24 +2,20 @@
 
 namespace App\Providers;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        Model::unguard();
+        if (app()->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
